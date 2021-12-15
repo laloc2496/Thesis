294351a3b67dcdbe4c7f267d8c99ca09ca19ba1a
BinhPham <binh.pham.quang@hcmut.edu.vn>
2021-12-15 22:16:01 +0700

    consumer

--- kafka_adafruit/consumer.py
+++ kafka_adafruit/consumer.py
@@ -2,12 +2,16 @@
 from pyspark.sql import SparkSession
 from utils import *
 
-spark= SparkSession.builder.master("local").getOrCreate()
+spark= SparkSession.builder.master(SPARK_MASTER).getOrCreate()
 
 df = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", BOOTSTRAP_SERVER) \
-  .option("subscribe", TOPIC_KAFKA) \
+  .option("subscribe", "topic1") \
   .load()
-df.selectExpr("CAST(value AS STRING)")
+data=df.selectExpr("CAST(value AS STRING)")
+
+
+data.writeStream.format("console").outputMode("append").start().awaitTermination()
+spark.stop()
