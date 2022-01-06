b497bec9c569e610be841305c4187571e9808241
BinhPham <binh.pham.quang@hcmut.edu.vn>
2022-01-06 21:26:21 +0700

    send request igrigation to server

---
+++ kafka_adafruit/motor.py
@@ -0,0 +1,14 @@
+
+from producer import connection_to_feed
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='This script used to send request igrigation')
+    parser.add_argument("--id", type=str)
+    parser.add_argument("--value", type=int)
+    args = parser.parse_args()
+    feed_id = args.id
+    value = int(args.value)
+    client = connection_to_feed('sensor')
+    client.publish('motor', value, feed_id)
