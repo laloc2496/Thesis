325519da14962dacf19fa66b08ab7d34b6dab194
BinhPham <binh.pham.quang@hcmut.edu.vn>
2022-01-17 23:43:21 +0700

    fix time irrigation in motor

--- kafka_adafruit/motor.py
+++ kafka_adafruit/motor.py
@@ -2,6 +2,7 @@
 from producer import connection_to_feed
 import argparse
 
+times_irrigation=[60,90,120,150]
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='This script used to send request irrigation')
@@ -11,4 +12,4 @@ if __name__ == "__main__":
     feed_id = args.id
     value = int(args.value)
     client = connection_to_feed('sensor')
-    client.publish('motor', value*100, feed_id)
+    client.publish('motor', times_irrigation[value]*2, feed_id)
