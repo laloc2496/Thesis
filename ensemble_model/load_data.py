e125e60887386b916aecb8b5de1c499d7e94e166
BinhPham <binh.pham.quang@hcmut.edu.vn>
2022-01-17 21:17:21 +0700

    add data train

--- ensemble_model/load_data.py
+++ ensemble_model/load_data.py
@@ -1,14 +1,16 @@
 import pandas as pd
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 # from sklearn import preprocessing
 # le = preprocessing.LabelEncoder()
 #df=pd.read_csv("/home/binh/data/data_sonar_train.csv")
 # le.fit(df.Class)
 # df['label'] = le.transform(df.Class)
-# train, test=train_test_split(df,test_size=0.3)
 
-# train.to_csv("/home/binh/data/data_sonar_train.csv")
-# test.to_csv("/home/binh/data/data_sonar_test.csv")
+df =pd.read_csv("data/data_new.csv")
+train, test=train_test_split(df,test_size=0.3)
+
+train.to_csv("data/data_train.csv")
+test.to_csv("data/data_test.csv")
 
 def get_train_data(spark,uri):
     df=pd.read_csv(uri)
