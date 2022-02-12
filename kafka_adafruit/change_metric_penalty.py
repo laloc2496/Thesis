2fea631aa89cea10b2636acada9e2779688041fa
BinhPham <binh.pham.quang@hcmut.edu.vn>
2022-02-12 22:48:36 +0700

    change metric for retrain

--- kafka_adafruit/change_metric_penalty.py
+++ kafka_adafruit/change_metric_penalty.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 
-dates = ["2022-01-24", "2022-01-25", "2022-01-26",
-         "2022-01-27", "2022-01-28", "2022-01-29"]
-models = ["bayes", "dt", "sensors", "svm"]
+# dates = ["2022-01-24", "2022-01-25", "2022-01-26",
+#          "2022-01-27", "2022-01-28", "2022-01-29"]
+# models = ["bayes", "dt", "sensors", "svm"]
 
 
-#option 0 [done] tuoi nghiem ngat nhat.
+dates = ["2022-01-31", "2022-02-01", "2022-02-02", "2022-02-03", "2022-02-04"]
+models = ["sensors", "voting"]
+# option 0 [done] tuoi nghiem ngat nhat.
 # Penalty:
 # ko lệch: 0
 # lệch <5: -1
@@ -14,7 +16,7 @@ models = ["bayes", "dt", "sensors", "svm"]
 # Lệch >10: -3
 
 
-#option 1 [done]
+# option 1 [done]
 # Penalty:
 # ko lệch: 0
 # lệch <10: -1
@@ -22,21 +24,18 @@ models = ["bayes", "dt", "sensors", "svm"]
 # Lệch >15: -3
 
 
-#option 2 [done]
+# option 2 [done]
 # Penalty:
 # ko lệch: 0
 # lệch <15: -1
 # Lệch 15-20: -2
 # Lệch >20: -3
 
-def get_name(date,model):
+def get_name(date, model):
     return f"evaluate_model/penalty/{date}_{model}.csv"
 
- 
- 
 
-
-def get_penalty(soil,threshhold):
+def get_penalty(soil, threshhold):
     if soil < threshhold:
         value = threshhold-soil
         if value < 15:
@@ -55,19 +54,19 @@ def get_penalty(soil,threshhold):
 
 
 for date in dates:
-    maxrow=None
+    maxrow = None
     print('-'*10)
     print(date)
     for model in models:
-        sum=0
-        count=0
-        df=pd.read_csv(get_name(date,model))
+        sum = 0
+        count = 0
+        df = pd.read_csv(get_name(date, model))
         if not maxrow:
-            maxrow= df.shape[0]
-        for _,row in df.iterrows():
-            if count>maxrow:
+            maxrow = df.shape[0]
+        for _, row in df.iterrows():
+            if count > maxrow:
                 continue
-            count+=1
-            sum+= get_penalty(row[2],row[3])        
-    
-        print(f"Model {model}: {sum}")
+            count += 1
+            sum += get_penalty(row[2], row[3])
+
+        print(f"Model {model}: {sum*3}")
